7e1173ab909836ef08a6b6a06f675de262b06851
FanTei <passmirgeb@gmail.com>
2022-08-29 16:50:11 +0300

    add messages locale and add locale to controller

--- chapter3/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ chapter3/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -5,6 +5,8 @@ import com.optimagrowth.license.service.LicenseService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -35,9 +37,11 @@ public class LicenseController {
     @PostMapping
     public ResponseEntity<String> createLicense(
             @PathVariable("organizationId") String organizationId,
-            @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.createLicense(request,
-                organizationId));
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language",required = false)
+            Locale locale){
+        return ResponseEntity.ok(licenseService.createLicense(
+                request, organizationId, locale));
     }
 
     @DeleteMapping(value = "/{licenseId}")
--- chapter3/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ chapter3/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -2,9 +2,13 @@ package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
 
+import java.util.Locale;
+
 public interface LicenseService {
     public License getLicense(String licenseId, String organizationId);
-    public String createLicense(License license, String organizationId);
+
+    public String createLicense(License license, String organizationId, Locale locale);
+
     public String updateLicense(License license, String organizationId);
 
     public String deleteLicense(String licenseId, String organizationId);

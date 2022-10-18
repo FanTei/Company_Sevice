144693ffa18c94c1ff4b0a552082cc6ac0c5466c
FanTei <79931683+FanTei@users.noreply.github.com>
2022-10-18 16:49:13 +0300

    Init

---
+++ chapter8/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -0,0 +1,49 @@
+package com.optimagrowth.license.model;
+
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+
+//POJO object
+@Data
+@Entity
+@Table(name = "licenses")
+public class License extends RepresentationModel<License> {
+
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+
+    private String description;
+
+    @Column(name = "organization_id", nullable = false)
+    private String organizationId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "license_type", nullable = false)
+    private String licenseType;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Transient
+    private String organizationName;
+
+    @Transient
+    private String contactName;
+    @Transient
+    private String contactPhone;
+    @Transient
+    private String contactEmail;
+
+
+
+    public License withComment(String comment) {
+        this.setComment(comment);
+        return this;
+    }
+}

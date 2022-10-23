2fba87a07352965e31932c483253be09fc3d63f4
FanTei <79931683+FanTei@users.noreply.github.com>
2022-10-23 19:53:57 +0300

    create secutiry and keycloack config

---
+++ chapter9/licensing-service/src/main/java/com/optimagrowth/license/config/KeycloakConfiguration.java
@@ -0,0 +1,28 @@
+package com.optimagrowth.license.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+public class KeycloakConfiguration {
+    private final KeycloakClientRequestFactory keycloakClientRequestFactory;
+
+    public KeycloakConfiguration(KeycloakClientRequestFactory keycloakClientRequestFactory) {
+        this.keycloakClientRequestFactory = keycloakClientRequestFactory;
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate() {
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }
+}

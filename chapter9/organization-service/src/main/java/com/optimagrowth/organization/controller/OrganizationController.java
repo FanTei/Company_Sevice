b35d9f709c367ddc3c1943780a0f6aa77f460d16
FanTei <79931683+FanTei@users.noreply.github.com>
2022-10-23 19:35:14 +0300

    add RolesAllowed

--- chapter9/organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
+++ chapter9/organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping(value = "v1/organization")
 public class OrganizationController {
@@ -16,24 +18,27 @@ public class OrganizationController {
         this.service = service;
     }
 
+    @RolesAllowed({"ADMIN","USER"})
     @GetMapping(value = "/{organizationId}")
     public ResponseEntity<Organization> getOrganization(@PathVariable String organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
-    @PutMapping(value = "/{organizationId}")
-    public void updateOrganization(@PathVariable String organizationId, @RequestBody Organization organization) {
+    @RolesAllowed({"ADMIN","USER"})
+    @PutMapping()
+    public void updateOrganization(@RequestBody Organization organization) {
         service.update(organization);
     }
 
+    @RolesAllowed({"ADMIN","USER"})
     @PostMapping
     public ResponseEntity<Organization> saveOrganization(@RequestBody Organization organization) {
         return ResponseEntity.ok(service.create(organization));
     }
-
+    @RolesAllowed("ADMIN")
     @DeleteMapping(value = "/{organizationId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrganization(@PathVariable String organizationId, @RequestBody Organization organization) {
-        service.delete(organization);
+    public void deleteOrganization(@PathVariable String organizationId) {
+        service.delete(organizationId);
     }
 }
